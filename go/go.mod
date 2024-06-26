6f4acb280ddbb2d8349991236a63a11c28f7e006
echo.ma <simple163@yahoo.com>
2024-06-26 16:25:50 +0800

    go2.go

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module codecamping_samples
+
+go 1.22.1
--- go/main/go2.go
+++ go/main/go2.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var sum = 0
-	for i := 1; i <= 100; i++ {
-		sum += i
-	}
+	var sum = accumulate(1, 100)
 	fmt.Println("1+2+...+100 =", sum)
+}
+
+func accumulate(start int, end int) int {
+	if end == start {
+		return start
+	}
+	return end + accumulate(start, end-1)
 }
