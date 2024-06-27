aa4734143b77a97e7223265a37d3a98bfcb5446f
echo.ma <simple163@yahoo.com>
2024-06-27 15:26:39 +0800

    fix plot1.py

--- python/matplotlib/plot1.py
+++ python/matplotlib/plot1.py
@@ -4,12 +4,16 @@ import numpy as np
 
 print(matplotlib.__version__)
 
+# draw a line from 
+# (x1, y1) -> (x2, y2)
+# e.g.
+# (0, 1) -> (6, 7)
 xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+ypoints = np.array([1, 7])
 
 print('xpoints', xpoints)
 print('ypoints', ypoints)
 
-plt.plot(xpoints, ypoints)
+plt.plot(xpoints, ypoints, 'o:r')
 plt.show()
 
