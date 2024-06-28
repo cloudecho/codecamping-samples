dcb85863c7e0174c1b4d59aa17eedf1fd37a9aa3
echo.ma <simple163@yahoo.com>
2024-06-28 16:52:37 +0800

    plot3d2

--- python/matplotlib/plot3d1.py
+++ python/matplotlib/plot3d1.py
@@ -15,7 +15,7 @@ ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x, y, z, c='g', marker='^')
 
 ax.set_xlabel('X')
-ax.set_ylabel('y')
+ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_title('3D Scatter Plot')
 
---
+++ python/matplotlib/plot3d2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+count = 200
+
+z = np.linspace(-3, 3, count)
+r = z**2 + 2
+theta = np.linspace(-6*np.pi, 6*np.pi,  count)
+x = r*np.sin(theta)
+y = r*np.cos(theta)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x, y, z, label='3D Line Plot')
+
+plt.show()
