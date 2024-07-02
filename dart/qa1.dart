f485be5a7a5a38adc5181f3f9cef21f3ba24919e
echo.ma <simple163@yahoo.com>
2024-07-02 15:49:10 +0800

    qa1.dart

---
+++ dart/qa1.dart
@@ -0,0 +1,11 @@
+void main() {
+  bool? b;
+  print(b);
+
+  bool a; //= true;
+  // a = false;
+
+  // The non-nullable local variable 'a' 
+  // must be assigned before it can be used.
+  print(a);
+}
--- python/matplotlib/plot3d2.py
+++ python/matplotlib/plot3d2.py
@@ -5,12 +5,12 @@ count = 200
 
 z = np.linspace(-3, 3, count)
 r = z**2 + 2
-theta = np.linspace(-6*np.pi, 6*np.pi,  count)
-x = r*np.sin(theta)
-y = r*np.cos(theta)
+theta = np.linspace(-6 * np.pi, 6 * np.pi, count)
+x = r * np.sin(theta)
+y = r * np.cos(theta)
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(x, y, z, label='3D Line Plot')
+ax = fig.add_subplot(111, projection="3d")
+ax.plot(x, y, z, label="3D Line Plot")
 
 plt.show()
