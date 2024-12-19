c5bdcaea027625e1efdc30a13ed7b813de21f454
echo.ma <simple163@yahoo.com>
2024-12-19 17:52:07 +0800

    add func factorial

--- go/main/go2.go
+++ go/main/go2.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	var sum = accumulate(1, 100)
 	fmt.Println("1+2+...+100 =", sum)
+
+	fmt.Println("5! =", factorial(5))
 }
 
 func accumulate(start int, end int) int {
@@ -12,4 +14,13 @@ func accumulate(start int, end int) int {
 		return start
 	}
 	return end + accumulate(start, end-1)
+}
+
+
+func factorial( n int) int {
+	if n == 0 {
+		return 1
+	} else {
+		return n * factorial(n - 1)
+	}
 }
