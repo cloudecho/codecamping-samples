40bcf5565d163c82c625da1e7982390896d7b9e4
echo.ma <simple163@yahoo.com>
2025-01-30 06:06:38 +0800

    c struct example

--- c/struct.c
+++ c/struct.c
@@ -12,11 +12,13 @@ int main()
     printf("Pointer p: %p\n", (void *)p);
     printf("aaa      : %p\n", (void *)aaa);
     printf("aaa[0]   : %p\n", (void *)&aaa[0]);
+    aaa[0].a  = 100;
+    printf("p->a     : %d\n", p->a);
 
-    printf("p++      : %p\n", (void *)++p);
+    printf("\np++      : %p\n", (void *)++p);
     printf("aaa[1]   : %p\n", (void *)&aaa[1]);
 
-    printf("p++      : %p\n", (void *)++p);
+    printf("\np++      : %p\n", (void *)++p);
     printf("aaa[2]   : %p\n", (void *)&aaa[2]);
     return 0;
 }
